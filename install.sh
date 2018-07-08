79dab3077be8e79b7f52d047f122b3b3e5f42981
DedSec CyberTeam TH <37942979+DedSecCyber@users.noreply.github.com>
2018-07-08 07:54:16 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+apt-get update -y && apt-get upgrade -y
+apt-get install python python2 -y
+apt-get install figlet -y
+apt-get install nmap -y
+apt-get install hydra -y
+pip install --upgrade pip
+pip2 install sqlmap
